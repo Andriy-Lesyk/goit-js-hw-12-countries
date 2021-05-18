1574c61b698f3f734dd811703ae860b0bab9aa8f
Andriy-Lesyk <74345435+Andriy-Lesyk@users.noreply.github.com>
2021-05-18 16:30:17 +0300

    Доделывает

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import debounce from 'lodash.debounce';
 import { name } from "file-loader";
 import fetchCountries from "./fetchCountries.js";
-import template1 from "./countries.hbs"
+import template1 from "./countries.hbs";
+import {notice, defaultModules} from '@pnotify/core';
+import * as PNotifyDesktop from '@pnotify/desktop';
+
+
 
 const inputCountry = document.querySelector('.inputCountry')
 console.log(inputCountry);
@@ -12,32 +16,36 @@ function renderCountry(country){
     const markup = template1(country)
     countryList.innerHTML = markup;
 }
-
+function cleanCountryList(){
+    countryList.removeChild
+}
 
 inputCountry.addEventListener('input', debounce(searchCountry, 500));
 
 function searchCountry(){
+countryList.innerHTML=""
 name=inputCountry.value  
 console.log(inputCountry.value )  
  fetchCountries(name)
 .then(country=>{console.log(country)
-    console.log(country[0].name)
-    if (country.length>1 && country.length<10){
-
+    if (country.length>1 && country.length<=10){
 const markup = []
 country.map(count=>{
     const newLi=document.createElement('li')
     console.log(count.name)
     newLi.append(count.name)
-    markup.push(newLi)
-    
+    markup.push(newLi)   
 });
 countryList.append(...markup)
-    } else if(country.length=1){
+    } else if(country.length===1){
         const markup=country.map(template1).join('')
         countryList.insertAdjacentHTML('beforeend', markup)
-    } else if (country.length=0){
-        countryList.removeChild()
+    } else if (country.length>10){
+        const myNotice = notice({
+            text: "Too many matches found. Please enter a more specific query!",
+          
+          });
+   
     }
 });
 }
